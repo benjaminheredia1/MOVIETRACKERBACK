95a4526f4341589202b06776eb32a6a97ecf14fa
Pablo Benjamin Heredia Ruiz <benjaherediaruiz@gmail.com>
2026-05-25 23:56:46 -0400

    (feature) agregando el crud completo para el usuario

--- internal/infrastructure/postgres/item_repository.go
+++ internal/infrastructure/postgres/item_repository.go
@@ -70,9 +70,44 @@ func (r *ItemRepository) GetAll(filters domain.Filters) ([]domain.ITEM, error) {
 
 func (r *ItemRepository) Add(item domain.ITEM) error {
 
-	query := `INSERT INTO items (nombre, descripcion) VALUES ($1, $2)`
+	query := `	INSERT INTO ITEMS (
+			tmdb_id, adult, backdrop_path, name, original_name, overview, 
+			poster_path, media_type, original_language, popularity, first_air_date, 
+			softcore, genre_ids, origin_country, vote_average, vote_count, 
+			list_id, status, comentary_user, calification_user, watched_at, added_at
+		) 
+		VALUES (
+			$1, $2, $3, $4, $5, $6, 
+			$7, $8, $9, $10, $11, 
+			$12, $13, $14, $15, $16, 
+			$17, $18, $19, $20, $21, $22
+		) RETURNING id`
 
-	_, err := r.db.Exec(query, item.Name, item.Overview)
+	var newID int
+	err := r.db.QueryRow(query,
+		item.TmdbID,
+		item.Adult,
+		item.BackdropPath,
+		item.Name,
+		item.OriginalName,
+		item.Overview,
+		item.PosterPath,
+		item.MediaType,
+		item.OriginalLanguage,
+		item.Popularity,
+		item.FirstAirDate,
+		item.Softcore,
+		item.GenreIDs,
+		item.OriginCountry,
+		item.VoteAverage,
+		item.VoteCount,
+		item.ListID,
+		item.Status,
+		item.ComentaryUser,
+		item.CalificationUser,
+		item.WatchedAt,
+		time.Now(),
+	).Scan(&newID)
 
 	if err != nil {
 		return err
@@ -82,11 +117,49 @@ func (r *ItemRepository) Add(item domain.ITEM) error {
 }
 
 func (r *ItemRepository) GetByID(id int) (*domain.ITEM, error) {
-	return nil, fmt.Errorf("metodo GetByID no implementado")
+	query := `SELECT 
+			id, tmdb_id, adult, backdrop_path, name, original_name, overview, 
+			poster_path, media_type, original_language, popularity, first_air_date, 
+			softcore, genre_ids, origin_country, vote_average, vote_count, 
+			list_id, status, comentary_user, calification_user, watched_at, added_at 
+		FROM items WHERE id = $1`
+	row := r.db.QueryRow(query, id)
+	var item domain.ITEM
+	row.Scan(
+		&item.ID,
+		&item.TmdbID,
+		&item.Adult,
+		&item.BackdropPath,
+		&item.Name,
+		&item.OriginalName,
+		&item.Overview,
+		&item.PosterPath,
+		&item.MediaType,
+		&item.OriginalLanguage,
+		&item.Popularity,
+		&item.FirstAirDate,
+		&item.Softcore,
+		&item.GenreIDs,
+		&item.OriginCountry,
+		&item.VoteAverage,
+		&item.VoteCount,
+		&item.ListID,
+		&item.Status,
+		&item.ComentaryUser,
+		&item.CalificationUser,
+		&item.WatchedAt,
+		&item.AddedAt,
+	)
+	return &item, nil
 }
 
 func (r *ItemRepository) MarkAsWatched(id int, rating int, watchedAt time.Time, commentary string) error {
-	return fmt.Errorf("metodo MarkAsWatched no implementado")
+	query := `UPDATE items SET status = 'watched', calification_user = $1, watched_at = $2, comentary_user = $3 WHERE id = $4`
+	_, err := r.db.Exec(query, rating, watchedAt, commentary, id)
+	if err != nil {
+		return fmt.Errorf("error actualizando el item: %w", err)
+	}
+	return nil
 }
 
 func (r *ItemRepository) Delete(id int) error {
